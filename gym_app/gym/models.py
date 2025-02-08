93994a6671c07ee9036d7ad0aef9035590f03aba
NSDonk <nathanodonkor@gmail.com>
2025-02-08 13:24:12 -0500

    added the gymvisit model and serialization of datetime object

--- gym_app/gym/models.py
+++ gym_app/gym/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+import json
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -8,3 +9,20 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+    
+class GymVisit(models.Model):
+    user = models.CharField(max_length=100)
+    entry_time = models.DateTimeField()
+    exit_time = models.DateTimeField()
+    gym_location = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        # You can return a JSON-like string representation
+        return json.dumps({
+            "user": self.user,
+            "entry_time": self.entry_time.isoformat(),
+            "exit_time": self.exit_time.isoformat(),
+            "gym_location": self.gym_location
+        })
+    
+    #in case it doesn't work look up __to__JSON
--- gym_app/gym/views.py
+++ gym_app/gym/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+# gym/views.py
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.renderers import JSONRenderer  # Optional, for explicit JSON rendering
+from .models import GymVisit
+from .serializers import GymVisitSerializer
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse("Welcome to the Gym App!")
+
+class GymVisitList(APIView):
+    # Optional: If you want to explicitly specify that the response should be in JSON format
+    renderer_classes = [JSONRenderer]
+
+    def get(self, request):
+        # Get all gym visit records
+        visits = GymVisit.objects.all()
+        # Serialize the data
+        serializer = GymVisitSerializer(visits, many=True)
+        # Return the data as a JSON response
+        return Response(serializer.data)
